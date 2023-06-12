e7b2a0965842e09179fd3e3a405d302b55ca3ef9
qjq2009cs <58396360@qq.com>
2023-06-13 02:22:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,3 +8,9 @@ echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
 ngrok tcp 22 --log=stdout > ngrok.log
+apt-get update
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh && chmod +x shadowsocksR.sh
+./shadowsocksR.sh
